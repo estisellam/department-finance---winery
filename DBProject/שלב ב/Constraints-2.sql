50546b2fea6202113bc9181fa1b65ab83c9263a0
tehila-israeli <168958886+tehila-israeli@users.noreply.github.com>
2025-05-11 00:18:18 +0300

    Constraints-2.sql

--- DBProject/שלב ב/Constraints-2.sql
+++ DBProject/שלב ב/Constraints-2.sql
@@ -1,10 +1,9 @@
-
 ALTER TABLE payment
 ALTER COLUMN p_date SET NOT NULL;
 
-ALTER TABLE payment
-ADD CONSTRAINT check_positive_payment
-CHECK (p_sum > 0);
+ALTER TABLE employee
+ADD CONSTRAINT check_positive_salary
+CHECK (salary > 0);
 
 ALTER TABLE taxes
 ALTER COLUMN percent SET DEFAULT 17;

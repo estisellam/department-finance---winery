a77ea4a0dcfeb124bda8391e5a851417c45bdffc
tehila-israeli <168958886+tehila-israeli@users.noreply.github.com>
2025-05-23 00:29:05 +0300

    Add files via upload

---
+++ DBProject/שלב ג/Integrate.sql
@@ -0,0 +1,26 @@
+
+-- מוסיפים שדות מיוחדים למדריכים
+ALTER TABLE employee ADD COLUMN employee_role TEXT;           -- למשל 'guide', 'worker'
+ALTER TABLE employee ADD COLUMN languages TEXT;      -- רק למדריכים
+ALTER TABLE employee ADD COLUMN guided BOOLEAN;      -- רק למדריכים
+
+
+ALTER TABLE payment ADD COLUMN payment_type TEXT;      -- מקור התשלום: 'visitor', 'employee', 'vehicle'...
+
+
+
+ALTER TABLE payment
+ADD CONSTRAINT fk_payment_employee
+FOREIGN KEY (e_id) REFERENCES employee(e_id);
+
+ALTER TABLE tour
+ADD CONSTRAINT fk_tour_guide
+FOREIGN KEY (guide_id) REFERENCES employee(e_id);
+
+ALTER TABLE payment ADD COLUMN visitorid INTEGER;
+
+ALTER TABLE payment
+ADD CONSTRAINT fk_payment_visitor
+FOREIGN KEY (visitorid) REFERENCES visitor(visitorid);
+
+
